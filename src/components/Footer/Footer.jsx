0fc033e07f1d27576d1e42cf40c2de89241fc358
Mykola <53128883+Nikolaus-B@users.noreply.github.com>
2024-03-20 13:37:17 +0200

    create component, new sprite

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,86 @@
+import Icon from "../Icon/Icon";
+import {
+  AddressContainer,
+  AddressWrapper,
+  FooterContainer,
+  IconContainer,
+  InfoContainer,
+  LinksContainer,
+  LinksWrapper,
+  SocialItem,
+  SocialLink,
+  SocialList,
+  StyledFooter,
+} from "./Footer.styled";
+
 const Footer = () => {
-  return <div>Hello</div>;
+  return (
+    <StyledFooter className="container">
+      <IconContainer>
+        <Icon width={48} height={72} iconId={"emblem"} />
+        <p>Ed UA</p>
+      </IconContainer>
+      <FooterContainer>
+        <SocialList>
+          <SocialItem>
+            <SocialLink href="#">
+              <Icon width={24} height={24} iconId={"instagram"} />
+            </SocialLink>
+          </SocialItem>
+          <SocialItem>
+            <SocialLink href="#">
+              <Icon width={24} height={24} iconId={"facebook"} />
+            </SocialLink>
+          </SocialItem>
+          <SocialItem>
+            <SocialLink href="#">
+              <Icon width={24} height={24} iconId={"phone"} />
+            </SocialLink>
+          </SocialItem>
+          <SocialItem>
+            <SocialLink href="#">
+              <Icon width={24} height={24} iconId={"telegram"} />
+            </SocialLink>
+          </SocialItem>
+        </SocialList>
+        <InfoContainer>
+          <LinksContainer>
+            <LinksWrapper>
+              <p>Офіційний веб-сайт</p>
+              <a href="#">Education UA</a>
+            </LinksWrapper>
+            <LinksWrapper>
+              <p>Права на всі матеріали належать</p>
+              <a href="#">Education UA</a>
+            </LinksWrapper>
+            <LinksWrapper>
+              <Icon width={20} height={20} iconId={"copyright"} />
+              <a href="">Education UA</a>
+              <p>2024 Всі права захищені</p>
+            </LinksWrapper>
+          </LinksContainer>
+          <AddressContainer>
+            <AddressWrapper>
+              <p>Гаряча лінія</p>
+              <a href="">0 (800) 000 000</a>
+            </AddressWrapper>
+            <AddressWrapper>
+              <p>Email</p>
+              <a href="">example@gmail.com</a>
+            </AddressWrapper>
+            <AddressWrapper>
+              <p>Телефон для дзвінків із-за кордону</p>
+              <a href="">044 000 00 00</a>
+            </AddressWrapper>
+            <AddressWrapper>
+              <p>Співпраця</p>
+              <a href="">example@gmail.com</a>
+            </AddressWrapper>
+          </AddressContainer>
+        </InfoContainer>
+      </FooterContainer>
+    </StyledFooter>
+  );
 };
 
 export default Footer;
--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -1,1 +1,37 @@
-// import styled from "styled-components";
+import styled from "styled-components";
+
+export const StyledFooter = styled.footer`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+`;
+
+export const IconContainer = styled.div`
+  /* position: absolute;
+  top: 20px;
+  width: 84px; */
+  display: none;
+`;
+
+export const FooterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SocialList = styled.ul`
+  display: flex;
+`;
+
+export const SocialItem = styled.li``;
+
+export const SocialLink = styled.a``;
+
+export const InfoContainer = styled.div``;
+
+export const LinksContainer = styled.div``;
+
+export const LinksWrapper = styled.div``;
+
+export const AddressContainer = styled.div``;
+
+export const AddressWrapper = styled.div``;
