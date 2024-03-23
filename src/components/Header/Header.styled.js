579d9e5fdfa0e67acd303bf01ed20bfaaa381888
svitlana-horodylova <horodylova.sv@gmail.com>
2024-03-23 09:10:50 +0000

    Update Header.styled.js

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,1 +1,46 @@
-// import styled from "styled-components";
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.header`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  height: 326px;
+`;
+
+export const BrandSection = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HeaderLogo = styled.div`
+  padding-top: 40px;
+  padding-bottom: 40px;
+  height: 180px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HeaderMenu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border: 1px solid black;
+`;
+export const IconContainer = styled.div`
+  display: flex;
+  top: 28px;
+  left: 84px;
+`;
+
+export const UserPanel = styled.div`
+  height: 21px;
+  width: 101px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Button = styled.button`
+`;
+
