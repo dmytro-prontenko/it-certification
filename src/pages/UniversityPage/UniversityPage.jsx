de6c6f32f65c10984bb78ed9fb2d77e0fc462942
domanskayelyzaveta <domanskaya_@ukr.net>
2024-03-31 14:55:19 +0200

    changed institution to universities

---
+++ src/pages/UniversityPage/UniversityPage.jsx
@@ -0,0 +1,25 @@
+// import { useSelector } from "react-redux";
+import {
+  ImageBottom,
+  StyledTableWrapper,
+} from "../../commonStyles/commonStyles";
+// import Table from "../../components/Table/Table";
+// import { tableData } from "../../redux/selectors/mainInfoSelectors";
+import { ImageTop } from "../../commonStyles/commonStyles";
+
+const UniversityPage = () => {
+  // const university = useSelector(tableData);
+  // const columns = ["№", "ЗВО", "Абревіатура", "Перелік прогам", "Сайт", "Дія"];
+
+  return (
+    <>
+      <StyledTableWrapper className="container">
+        <ImageTop></ImageTop>
+        {/* <Table view={"Перелік ЗВО"} data={university} columns={columns} /> */}
+        <ImageBottom></ImageBottom>
+      </StyledTableWrapper>
+    </>
+  );
+};
+
+export default UniversityPage;
