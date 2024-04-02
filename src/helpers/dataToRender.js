68e11f09db47eabc9f2d58e81be8dc5237834154
Gunnar97 <vadymshatov@proton.me>
2024-04-02 18:31:32 +0300

    add discipline

--- src/helpers/dataToRender.js
+++ src/helpers/dataToRender.js
@@ -199,7 +199,7 @@ export const departmentTableData = (columns, data) => {
   return dataArrays;
 };
 
-export const disciplineBlockLevelTableData = (columns, data) => {
+export const disciplineBlockTableData = (columns, data) => {
   function objectToArray(index, columns, data) {
     const dataArray = [index + 1];
     for (let key of columns) {
@@ -224,7 +224,7 @@ export const disciplineBlockLevelTableData = (columns, data) => {
   return dataArrays;
 };
 
-export const disciplineGroupLevelTableData = (columns, data) => {
+export const disciplineGroupTableData = (columns, data) => {
   function objectToArray(index, columns, data) {
     const dataArray = [index + 1];
     for (let key of columns) {
@@ -240,6 +240,46 @@ export const disciplineGroupLevelTableData = (columns, data) => {
           break;
         case "Дисципліни":
           dataArray.push(data.discipline_url);
+          break;
+        default:
+          break;
+      }
+    }
+    return dataArray;
+  }
+
+  const dataArrays = [];
+  data?.forEach((obj, index) => {
+    dataArrays.push(objectToArray(index, columns, obj));
+  });
+  return dataArrays;
+};
+
+export const disciplineTableData = (columns, data) => {
+  function objectToArray(index, columns, data) {
+    const dataArray = [index + 1];
+    for (let key of columns) {
+      switch (key) {
+        case "Дисципліна":
+          dataArray.push(data.name);
+          break;
+        case "Викладач":
+          dataArray.push(data.teacher.name);
+          break;
+        case "Блок":
+          dataArray.push(data.discipline_block.name);
+          break;
+        case "Група":
+          dataArray.push(data.discipline_group.name);
+          break;
+        case "Програма":
+          dataArray.push(data.education_program.program_url);
+          break;
+        case "Сілабус":
+          dataArray.push(data.discipline_group.syllabus_url);
+          break;
+        case "РП":
+          dataArray.push(data.education_plan_url);
           break;
         default:
           break;
--- src/pages/DisciplinePage/DisciplinePage.jsx
+++ src/pages/DisciplinePage/DisciplinePage.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   ImageBottom,
   StyledTableWrapper,
@@ -6,9 +6,27 @@ import {
 import Table from "../../components/Table/Table";
 import { tableData } from "../../redux/selectors/mainInfoSelectors";
 import { ImageTop } from "../../commonStyles/commonStyles";
+import { useLocation } from "react-router-dom";
+import { selectCurrentPage } from "../../redux/selectors/serviceSelectors";
+import { useEffect } from "react";
+import { getTableDataThunk } from "../../redux/thunk/mainInfoThunks";
+import { SIZE } from "../../service/constant";
+import { disciplineTableData } from "../../helpers/dataToRender";
 
 const DisciplinePage = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
   const discipline = useSelector(tableData);
+  const currentPage = useSelector(selectCurrentPage);
+
+  useEffect(() => {
+    dispatch(
+      getTableDataThunk({
+        endPoint: `${location.pathname}`,
+        getParams: { page: currentPage, size: SIZE },
+      })
+    );
+  }, []);
   const columns = [
     "№",
     "Дисципліна",
@@ -21,11 +39,17 @@ const DisciplinePage = () => {
     "Дія",
   ];
 
+  const dataToRender = disciplineTableData(columns, discipline.content);
+
   return (
     <>
       <StyledTableWrapper className="container">
         <ImageTop></ImageTop>
-        <Table view={"Перелік дисциплін"} data={discipline} columns={columns} />
+        <Table
+          view={"Перелік дисциплін"}
+          data={dataToRender}
+          columns={columns}
+        />
         <ImageBottom></ImageBottom>
       </StyledTableWrapper>
     </>
